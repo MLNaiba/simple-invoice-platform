84743fa43cb2945705e175c22dace2e763149a0c
Mihai-Lucian Naiba <1135534+MLNaiba@users.noreply.github.com>
2026-08-31 12:33:07 +0300

    Initial project setup

---
+++ src/main/java/io/github/MLNaiba/simpleinvoice/SimpleinvoiceApplication.java
@@ -0,0 +1,13 @@
+package io.github.MLNaiba.simpleinvoice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleinvoiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleinvoiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/MLNaiba/simpleinvoice/SimpleinvoiceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.MLNaiba.simpleinvoice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleinvoiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
