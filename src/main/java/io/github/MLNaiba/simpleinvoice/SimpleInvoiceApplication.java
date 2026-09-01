5cfb1ea775362e87b718ec55b7d3133b501b409a
Mihai-Lucian Naiba <1135534+MLNaiba@users.noreply.github.com>
2026-09-01 17:32:01 +0300

    Renamed the main class (capitalized the I in invoice) and the main test class

--- src/main/java/io/github/MLNaiba/simpleinvoice/SimpleInvoiceApplication.java
+++ src/main/java/io/github/MLNaiba/simpleinvoice/SimpleInvoiceApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SimpleinvoiceApplication {
+public class SimpleInvoiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SimpleinvoiceApplication.class, args);
+		SpringApplication.run(SimpleInvoiceApplication.class, args);
 	}
 
 }
