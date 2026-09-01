47c7201cba653d38693e3cefc4268e034dd86f16
Mihai-Lucian Naiba <1135534+MLNaiba@users.noreply.github.com>
2026-09-01 14:27:23 +0300

    Added product domain model

---
+++ src/main/java/io/github/MLNaiba/simpleinvoice/domain/Product.java
@@ -0,0 +1,23 @@
+package io.github.MLNaiba.simpleinvoice.domain;
+
+import lombok.*;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+@ToString
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Document("Product")
+public class Product {
+    @Id
+    private String id;
+
+    private String name;
+
+    private BigDecimal price;
+}
