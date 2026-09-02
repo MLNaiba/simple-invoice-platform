dfe58de715e6d8f95b9a8fea80f5bb8a5c958e3d
Mihai-Lucian Naiba <1135534+MLNaiba@users.noreply.github.com>
2026-09-02 14:26:47 +0300

    Added InvalidInvoiceStateTransitionException handler to the global handler

--- src/main/java/io/github/MLNaiba/simpleinvoice/exception/GlobalExceptionHandler.java
+++ src/main/java/io/github/MLNaiba/simpleinvoice/exception/GlobalExceptionHandler.java
@@ -32,6 +32,12 @@ public class GlobalExceptionHandler {
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
     }
 
+    @ExceptionHandler(InvalidInvoiceStateTransitionException.class)
+    public ResponseEntity<ErrorResponse> handleInvalidInvoiceStateTransitionException(
+            InvalidInvoiceStateTransitionException exception) {
+        return buildErrorResponse(HttpStatus.CONFLICT, exception.getMessage());
+    }
+
     // ---
 
     private ResponseEntity<ErrorResponse> buildErrorResponse(HttpStatus status, String message) {
