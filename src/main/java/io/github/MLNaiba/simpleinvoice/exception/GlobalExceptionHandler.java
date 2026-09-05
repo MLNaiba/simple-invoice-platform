1cbde7be86d5b6305ce7a10bed9230775bbb39f9
Mihai-Lucian Naiba <1135534+MLNaiba@users.noreply.github.com>
2026-09-05 16:13:37 +0300

    Added BadCredentialsException handler to the global handler

--- src/main/java/io/github/MLNaiba/simpleinvoice/exception/GlobalExceptionHandler.java
+++ src/main/java/io/github/MLNaiba/simpleinvoice/exception/GlobalExceptionHandler.java
@@ -2,6 +2,7 @@ package io.github.MLNaiba.simpleinvoice.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -42,6 +43,12 @@ public class GlobalExceptionHandler {
     public ResponseEntity<ErrorResponse> handleResourceAlreadyExistsException(
             ResourceAlreadyExistsException exception) {
         return buildErrorResponse(HttpStatus.CONFLICT, exception.getMessage());
+    }
+
+    @ExceptionHandler(BadCredentialsException.class)
+    public ResponseEntity<ErrorResponse> handleBadCredentialsException(
+            BadCredentialsException exception) {
+        return buildErrorResponse(HttpStatus.UNAUTHORIZED, exception.getMessage());
     }
 
     // ---
