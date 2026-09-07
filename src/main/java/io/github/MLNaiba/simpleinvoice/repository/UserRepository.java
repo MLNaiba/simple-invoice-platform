0dae90237a83ef0d79cf5e0294e87fa666c2e42c
Mihai-Lucian Naiba <1135534+MLNaiba@users.noreply.github.com>
2026-09-07 14:32:29 +0300

    Added existsByRole function to the user repository

--- src/main/java/io/github/MLNaiba/simpleinvoice/repository/UserRepository.java
+++ src/main/java/io/github/MLNaiba/simpleinvoice/repository/UserRepository.java
@@ -1,6 +1,7 @@
 package io.github.MLNaiba.simpleinvoice.repository;
 
 import io.github.MLNaiba.simpleinvoice.domain.User;
+import io.github.MLNaiba.simpleinvoice.domain.UserRole;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
@@ -12,4 +13,6 @@ public interface UserRepository extends MongoRepository<User, String> {
     Optional<User> findByUsername(String username);
 
     boolean existsByUsername(String username);
+
+    boolean existsByRole(UserRole role);
 }
