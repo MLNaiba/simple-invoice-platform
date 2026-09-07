edfde4b2af992549ffebad9ccb6e670fc8dbb291
Mihai-Lucian Naiba <1135534+MLNaiba@users.noreply.github.com>
2026-09-07 15:59:40 +0300

    Added an additional clearContext call to ensure the clearing is done at the end of the JWT authentication filter tests as well

--- src/test/java/io/github/MLNaiba/simpleinvoice/security/JwtAuthenticationFilterTests.java
+++ src/test/java/io/github/MLNaiba/simpleinvoice/security/JwtAuthenticationFilterTests.java
@@ -6,6 +6,7 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -203,5 +204,10 @@ public class JwtAuthenticationFilterTests {
                 .password(PASSWORD)
                 .roles(USER_ROLE)
                 .build();
+    }
+
+    @AfterEach
+    public void tearDown() {
+        SecurityContextHolder.clearContext();
     }
 }
