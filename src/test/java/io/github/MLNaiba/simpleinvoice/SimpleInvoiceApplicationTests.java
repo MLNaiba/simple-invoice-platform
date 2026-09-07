108b949813051e15feae8bd2d84f3cc9b8b65101
Mihai-Lucian Naiba <1135534+MLNaiba@users.noreply.github.com>
2026-09-07 14:34:09 +0300

    Made the minimal smoke test use the test configuration

--- src/test/java/io/github/MLNaiba/simpleinvoice/SimpleInvoiceApplicationTests.java
+++ src/test/java/io/github/MLNaiba/simpleinvoice/SimpleInvoiceApplicationTests.java
@@ -2,7 +2,9 @@ package io.github.MLNaiba.simpleinvoice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("test")
 @SpringBootTest
 class SimpleInvoiceApplicationTests {
 
